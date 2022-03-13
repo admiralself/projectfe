706051a1e00897c3eeb7e9a12317e147a3bf6c8d
Admiral <admiralself@gmail.com>
2022-03-13 16:56:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import MovieList from "./Components/MovieList";
+import { BsFillBugFill } from "react-icons/bs";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route exact path="/admin" component={MovieList}  />
+          <BsFillBugFill path="/admin" component={MovieList}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
